3919e7b81503be90a90405be8355c33f85b19b9c
s.sukhomlyn <samuil.sukhomlyn@gmail.com>
2026-02-17 11:36:27 +0200

    Initial commit

---
+++ src/IdeExtensionsApplication.cs
@@ -0,0 +1,22 @@
+namespace Loupedeck.IdeExtensionsPlugin
+{
+    using System;
+
+    // This class can be used to connect the Loupedeck plugin to an application.
+
+    public class IdeExtensionsApplication : ClientApplication
+    {
+        public IdeExtensionsApplication()
+        {
+        }
+
+        // This method can be used to link the plugin to a Windows application.
+        protected override String GetProcessName() => "";
+
+        // This method can be used to link the plugin to a macOS application.
+        protected override String GetBundleName() => "";
+
+        // This method can be used to check whether the application is installed or not.
+        public override ClientApplicationStatus GetApplicationStatus() => ClientApplicationStatus.Unknown;
+    }
+}
